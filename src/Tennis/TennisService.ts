aecc1cf8f20a44b7bd19a94d91896614d6c9a790
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-01-13 00:02:29 +0800

    finish tennis Kata

--- src/Tennis/TennisService.ts
+++ src/Tennis/TennisService.ts
@@ -1,5 +1,66 @@
 export class TennisService {
-    Score() {
-        return "love all";
-    }
+	private _p1Score: number = 0;
+	private scoreMapping: { [key: number]: string } = {
+		0: "love",
+		1: "fifteen",
+		2: "thirty",
+		3: "forty"
+	};
+	private _p2Score: number = 0;
+
+	Score() {
+		return this.isDifferent()
+			   ? this.isReadyForGamePoint()
+				 ? this.isGamePoint()
+				   ? this.winResult()
+				   : this.AdvResult()
+				 : this.DifferentScoreResult()
+			   : this.isReadyForGamePoint()
+				 ? this.Deuce()
+				 : this.sameScoreResult();
+	}
+
+	private sameScoreResult() {
+		return `${this.scoreMapping[this._p1Score]} all`;
+	}
+
+	private Deuce() {
+		return "deuce";
+	}
+
+	private DifferentScoreResult() {
+		return `${this.scoreMapping[this._p1Score]} ${this.scoreMapping[this._p2Score]}`;
+	}
+
+	private AdvResult() {
+		return `${(this._p1Score > this._p2Score
+				   ? "p1"
+				   : "p2")} adv`;
+	}
+
+	private winResult() {
+		return `${(this._p1Score > this._p2Score
+				   ? "p1"
+				   : "p2")} win`;
+	}
+
+	private isGamePoint() {
+		return Math.abs(this._p1Score - this._p2Score) === 2;
+	}
+
+	private isReadyForGamePoint() {
+		return this._p1Score > 3 || this._p2Score > 3;
+	}
+
+	private isDifferent() {
+		return this._p1Score != this._p2Score;
+	}
+
+	public P1Goal() {
+		this._p1Score += 1;
+	}
+
+	public P2Goal() {
+		this._p2Score += 1
+	}
 }
--- src/Tennis/__tests__/TennisService.spec.ts
+++ src/Tennis/__tests__/TennisService.spec.ts
@@ -1,11 +1,94 @@
-import {describe, expect, test} from "vitest";
+import {it, beforeEach, describe, expect, test} from "vitest";
 import {TennisService} from "../TennisService";
 
 describe('For tennis service', () => {
+	let service: TennisService;
+	beforeEach(() => {
+		service = new TennisService();
+	});
 
-    test('love all', async () => {
-        var service = new TennisService();
-        expect(service.Score()).toBe("love all");
-    });
+	it('love all', async () => {
+		expect(service.Score()).toBe("love all");
+	});
+
+	function setP1Score(score: number) {
+		for (let i = 0; i < score; i++) {
+
+			service.P1Goal();
+		}
+	}
+
+	it('should fifteen love', () => {
+		setP1Score(1);
+		expect(service.Score()).toBe("fifteen love");
+	});
+
+	it('should thirty love', () => {
+		setP1Score(2);
+		expect(service.Score()).toBe("thirty love");
+	});
+
+	it('should forty love', () => {
+		setP1Score(3);
+		expect(service.Score()).toBe("forty love");
+	});
+
+	function setP2Score(score: number) {
+		for (let i = 0; i < score; i++) {
+			service.P2Goal();
+		}
+	}
+
+	it('should love fifteen', () => {
+		setP2Score(1);
+		expect(service.Score()).toBe("love fifteen");
+	});
+
+	it('should love thirty', () => {
+		setP2Score(2);
+		expect(service.Score()).toBe("love thirty");
+	});
+
+	it('should love forty', () => {
+		setP2Score(3)
+		expect(service.Score()).toBe("love forty");
+	});
+
+	it('should p1 adv', () => {
+		setP1Score(4);
+		setP2Score(3);
+		expect(service.Score()).toBe("p1 adv");
+	});
+
+	it('should p2 adv', () => {
+		setP1Score(3);
+		setP2Score(4);
+		expect(service.Score()).toBe("p2 adv");
+	});
+
+	it('should p1 win', () => {
+		setP1Score(5);
+		setP2Score(3);
+		expect(service.Score()).toBe("p1 win");
+	});
+
+	it('should p2 win', () => {
+		setP1Score(3);
+		setP2Score(5);
+		expect(service.Score()).toBe("p2 win");
+	});
+
+	it('should deuce', () => {
+		setP1Score(4);
+		setP2Score(4);
+		expect(service.Score()).toBe("deuce");
+	});
+
+	it('should fifteen all', () => {
+		setP1Score(1);
+		setP2Score(1);
+		expect(service.Score()).toBe("fifteen all");
+
+	});
 
 })
